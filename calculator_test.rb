5fd1f2c819ac3c3f7795de89e67e66f42db22c70
Jeremy <jsheridanwells@gmail.com>
2018-03-10 22:30:28 -0600

    refactored

--- calculator_test.rb
+++ calculator_test.rb
@@ -3,38 +3,50 @@ require_relative 'calculator'
 
 class TestCalculator < Minitest::Test
 
-  def setup
-    puts "set up"
-    @calc = Calculator.new
+  describe Calculator do
+    before do
+      @calculator = Calculator.new
+    end
+
+    describe "when given two integers as arguments" do
+      it "returns an integer" do
+        assert @calculator.is_a? Calculator
+      end
+    end
+
+    describe "when given two random numbers to add" do
+      it "returns their sum" do
+        a = rand(100)
+        b = rand(100)
+        assert_equal a + b, @calculator.add(a,b)
+      end
+    end
+
+    describe "when given two random numbers to subtract" do
+      it "returns their difference" do
+        a = rand(100)
+        b = rand(100)
+        assert_equal a - b, @calculator.subtract(a,b)
+      end
+    end
+
+    describe "when given two random numbers to multiply" do
+      it "returns their product" do
+        a = rand(100)
+        b = rand(100)
+        assert_equal a * b, @calculator.multiply(a,b)  
+      end
+    end
+    
+    describe "when given two random numbers to divide" do
+      it "returns their quotient" do
+        a = rand(100)
+        b = rand(100)
+        assert_in_delta a / b, @calculator.divide(a,b)
+      end
+    end
+
+    
   end
-
-
-  def test_output_is_integer
-    assert_kind_of Integer, @calc.add(rand(10),rand(10))
-  end
-
-  def test_add
-    assert_equal 4, @calc.add(2,2)
-  end
-
-  # Write test methods for subtract, multiply, and divide
-  def test_subtract
-    assert_equal 4, @calc.subtract(8,4)
-  end
-
-  def test_multiply
-    assert_equal 16, @calc.multiply(2,8)
-  end
-
-  def test_divide
-    assert_equal 1, @calc.divide(4,4)
-  end
-
-
-  def teardown
-    puts "tear down"
-  end
-
-
 
 end
